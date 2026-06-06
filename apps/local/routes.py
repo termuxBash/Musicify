310f1075cc98e8431f153476ca32274ab7ec9e02
Aaron Gomes <aaron.gomes25@spit.ac.in>
2026-06-06 21:51:31 +0530

    image fix

--- apps/local/routes.py
+++ apps/local/routes.py
@@ -58,7 +58,7 @@ def enqueue():
         "local",
         {
             "title": song["title"],
-            #"thumbnail": song["thumbnail"],
+            "thumbnail": "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext x='50' y='65' text-anchor='middle' font-size='60' font-family='sans-serif'%3E🎵️%3C/text%3E%3C/svg%3E",
             "url": url
         }
     )
